0a532bc741751df8c13037dcb0d273f15ff4d086
danny29ced-posp <danny29ced@gmail.com>
2026-09-17 19:36:48 -0500

    Add files via upload

--- app/src/main/java/com/tuempresa/possystem/ui/componentes/TabsYHeaderEco.kt
+++ app/src/main/java/com/tuempresa/possystem/ui/componentes/TabsYHeaderEco.kt
@@ -11,9 +11,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
